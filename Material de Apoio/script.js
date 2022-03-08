db122fe6ca377962f26bd986082e6ad8cbf1aa0d
Daiane de Souza Alexandre <dsalexandre83@gmail.com>
2022-03-08 11:31:32 -0300

    Inserção de funções no Javascript

--- Material de Apoio/script.js
+++ Material de Apoio/script.js
@@ -8,15 +8,41 @@ function  Todo(name) {
 function addNewTodoWithName(name){
     var t = new Todo(name);
     todos.push(t);
+    saveTodos();
+
+
+
+
+
+
+
+    
 }
 
 function removeTodoAtIndex(index){
     todos.splice(index,1);
+    saveTodos();
 }
 
 function getTodoAtIndex(index) {
     return todos[index];
 }
+
+function saveTodos() {
+    var str = JSON.stringify(todos);
+    localStorage.setItem("todos",str);
+}
+
+function getTodos() {
+   var str = localStorage.getItem();
+   todos = JSON.parse(str);
+   if (!todos) {
+       todos = [];
+   } 
+}
+
+getTodos();
+listTodos();
 
 function  listTodos() {
     var html = "";
@@ -27,4 +53,14 @@ function  listTodos() {
         var completed = todo.completed;
         html += "<li>"+name+"<span>"+completed+"</span></li>";
     }
+    $("#list-todos").html(html);
 }
+
+$("#add-todo-form").submit(function (event) {
+    event.preventDefault();
+
+    var name = $("#todo-name").val();
+
+    addNewTodoWithName(name);
+    listTodos();
+});
